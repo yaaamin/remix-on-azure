f6412cd3193dc890529680cda6d24b05ec399f2b
yaaamin <yaaamin0914@gmail.com>
2022-03-03 02:07:00 +0500

    Update github.jsx

--- app/routes/auth/github.jsx
+++ app/routes/auth/github.jsx
@@ -1,5 +1,7 @@
 import { authenticator } from '../../utils/auth.server'
 
+export let loader = () => redirect('/login')
+
 export let action = async ({ request }) => {
   return await authenticator.authenticate('github', request, {
     successRedirect: '/secret',
