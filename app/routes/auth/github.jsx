3247f6a44a5bde580e828d999f5a8b5ca664d61f
yaaamin <yaaamin0914@gmail.com>
2022-03-03 08:40:21 +0500

    Update github.jsx

--- app/routes/auth/github.jsx
+++ app/routes/auth/github.jsx
@@ -1,18 +1,18 @@
 import { authenticator } from '../../utils/auth.server'
-import {redirect} from 'remix'
+import { redirect } from 'remix'
+import React from 'react'
 
-export let loader = () => redirect('/login')
+export let loader = async ({request}) => {
+  // return redirect('/login')
+  return await authenticator.authenticate('github', request)
+}
 
 export let action = async ({ request }) => {
   return await authenticator.authenticate('github', request)
 }
 
-import React from 'react'
-
 function Github() {
-  return (
-    <div>Github Auth</div>
-  )
+  return <div>Github Auth</div>
 }
 
 export default Github
